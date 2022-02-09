c78ad4678fdb259f90d503405ce1bcab3337f7ab
Dora Hoeppner <dora.hoeppner@gmail.com>
2022-02-09 19:48:12 +0100

    Create card component

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -3,4 +3,20 @@ export default {
 };
 
 export const Default = () => /*html */ `
-  <article class="card">hi!</article>`;
+<article class="card">
+      <img src="../images/bookmark.svg" />
+      <h1>QUESTION:</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odit,
+        temporibus atque optio ex obcaecati doloremque similique maxime
+        perferendis molestias minima rerum harum. Similique, a distinctio itaque
+        eius delectus minus!
+      </p>
+      <button>SHOW ANSWER</button>
+      <div>
+        <span class="tag">topic abc</span>
+        <span class="tag">topic def</span>
+        <span class="tag">topic ghi</span>
+        <span class="tag">topic jkl</span>
+      </div>
+    </article>`;
---
+++ style/components/Tag.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: "Tag",
+};
+
+export const Default = () => /*html */ `
+          <span class="tag">topic abc</span>
+        `;
